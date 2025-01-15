d79a4cfce2767077c4520317c94419aee2aaaaf5
Simone Trinh <trinhthelong.bav@gmail.com>
2025-01-15 13:49:56 +0700

    add database package:

---
+++ context/database.py
@@ -0,0 +1,23 @@
+import sqlite3
+import os
+
+db_context: sqlite3.Connection = None
+
+currnet_dir = os.path.dirname(os.path.abspath(__file__))
+project_dir = os.path.abspath(os.path.join(currnet_dir, '..'))
+db_path = os.path.join(project_dir, 'main_todo.db')
+
+def connect_db() -> sqlite3.Connection:
+    global db_context
+    if(db_context is None):
+        db_context = sqlite3.connect(db_path)
+        print("Connect to db")
+    return db_context
+
+    # cursor = connect_db().cursor()
+    # cursor.execute('SELECT * FROM Tasks')
+
+    # for row in cursor.fetchall():
+    #     print(row)
+
+    # db_context.close()
